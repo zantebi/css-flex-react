79026fa5423b9910b739342e2560ee4183e0f697
Zedi Antebi <zedi.antebi@gmail.com>
2021-07-31 07:08:59 +0300

    examples for flex

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div className="container-1">
+            <div className="box-1">
+              <h3>Box one</h3>
+              <p>this is box one</p>
+            </div>
+            <div className="box-2">
+              <h3>Box two</h3>
+              <p>this is box two</p>
+            </div>
+            <div className="box-3">
+              <h3>Box three</h3>
+              <p>this is box three</p>
+            </div>
+          </div>
+          <div className="container-2">
+            <div className="container-2-box">
+              <h3>Box 2 one</h3>
+              <p>this is box one</p>
+            </div>
+            <div className="container-2-box">
+              <h3>Box 2 two</h3>
+              <p>this is box two</p>
+            </div>
+            <div className="container-2-box">
+              <h3>Box 2 three</h3>
+              <p>this is box three</p>
+            </div>
+          </div>
+          <div className="container-3">
+            <div className="container-3-box">
+              <h3>Box 2 one</h3>
+              <p>this is box one</p>
+            </div>
+            <div className="container-3-box">
+              <h3>Box 2 two</h3>
+              <p>this is box two</p>
+            </div>
+            <div className="container-3-box">
+              <h3>Box 2 three</h3>
+              <p>this is box three</p>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
